fe641ff60f99f72ead773243a01688276fc9fe94
Thiago Veiga <thiago.veiga@totvs.com.br>
2020-05-24 17:02:35 -0300

    add annotations

--- src/main/java/br/com/fiap/ecommerce/produto/ProdutoController.java
+++ src/main/java/br/com/fiap/ecommerce/produto/ProdutoController.java
@@ -1,7 +1,9 @@
 package br.com.fiap.ecommerce.produto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ProdutoController {
 
 	@Autowired
--- src/main/java/br/com/fiap/ecommerce/produto/ProdutoRepository.java
+++ src/main/java/br/com/fiap/ecommerce/produto/ProdutoRepository.java
@@ -1,5 +1,8 @@
 package br.com.fiap.ecommerce.produto;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProdutoRepository {
 
 }
