443f2d8a93be78a450eb355a4cc60d39a9970e2c
Thiago Veiga <thiago.veiga@totvs.com.br>
2020-05-28 22:08:00 -0300

    documentacao e crud

--- src/main/java/br/com/fiap/ecommerce/produto/ProdutoController.java
+++ src/main/java/br/com/fiap/ecommerce/produto/ProdutoController.java
@@ -4,11 +4,15 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,5 +32,27 @@ public class ProdutoController {
 	@PostMapping
 	public ResponseEntity<Produto> create(@RequestBody Produto produto) {
 		return ResponseEntity.ok(produtoService.save(produto));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto produto) {
+		try {
+			return ResponseEntity.ok(produtoService.update(id, produto));	
+		} catch (Exception e) {
+			log.error(String.format("nao foi possivel atualizar o produto %d com valores: %s", id, produto));
+	        return ResponseEntity.badRequest().build();
+		}
+		
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Produto> delete(@PathVariable Long id) {
+		
+		try {
+			return ResponseEntity.ok(produtoService.deleteById(id));	
+		} catch (Exception e) {
+			log.error(String.format("nao foi possivel deletar o produto %d ", id));
+	        return ResponseEntity.badRequest().build();
+		}
 	}
 }
--- src/main/java/br/com/fiap/ecommerce/produto/ProdutoService.java
+++ src/main/java/br/com/fiap/ecommerce/produto/ProdutoService.java
@@ -1,9 +1,11 @@
 package br.com.fiap.ecommerce.produto;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProdutoService {
@@ -17,5 +19,29 @@ public class ProdutoService {
 
 	public Produto save(Produto produto) {
 		return produtoRepository.save(produto);
+	}
+
+	public Produto deleteById(Long id) {
+		Optional<Produto> optional = produtoRepository.findById(id);
+
+		if (!optional.isPresent())
+			throw new IllegalArgumentException(String.format("Produto com id %d não encontrado para ser removido ", id));
+
+		produtoRepository.deleteById(id);
+		return optional.get();
+	}
+
+	public Produto update(Long id, Produto produto) {
+
+		Optional<Produto> optional = produtoRepository.findById(id);
+
+		if (!optional.isPresent())
+			throw new IllegalArgumentException(
+					String.format("Produto com id %d não encntrado para efetuar atualizacao ", id));
+
+		Produto temp = optional.get();
+		//setar atributos do produto
+		return produtoRepository.save(temp);
+
 	}
 }
